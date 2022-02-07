50679e5769f7af3f828465ff124f84fcb360015c
Mirko Bertone <bertonemirko@gmail.com>
2022-02-07 17:42:57 +0100

    Installed material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MonitorComponent } from './components/monitor/monitor.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 const config: SocketIoConfig = { url: 'http://localhost:3333', options: {} };
 @NgModule({
@@ -16,6 +17,8 @@ const config: SocketIoConfig = { url: 'http://localhost:3333', options: {} };
     BrowserModule,
     AppRoutingModule,
     SocketIoModule.forRoot(config),
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
