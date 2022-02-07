4272487be9c88ea41c6f0d0784f637e39e1bc3a8
Mirko Bertone <bertonemirko@gmail.com>
2022-02-07 12:39:19 +0100

    Generate monitor component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MonitorComponent } from './components/monitor/monitor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "", component: MonitorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonitorComponent } from './components/monitor/monitor.component';
 
 
 const config: SocketIoConfig = { url: 'http://localhost:3333', options: {} };
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitorComponent
   ],
   imports: [
     BrowserModule,
